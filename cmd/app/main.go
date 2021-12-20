c9d16c3ca0607a17a82879f3d453e4271e3e6455
Pavel <korsun4@mail.ru>
2021-12-20 22:18:22 +0300

    add viper

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,12 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/pavelkorshunov/go-skeleton/internal/app"
 	v1 "github.com/pavelkorshunov/go-skeleton/internal/controllers/http/v1"
+	"github.com/spf13/viper"
 )
-
-var Version = "develop"
 
 func main() {
 	router := gin.Default()
 	router.GET("/", v1.HelloController)
-	router.Run(":8080")
+
+	router.Run(viper.GetString("APP_URL"))
 }
--- internal/app/init.go
+++ internal/app/init.go
@@ -1,7 +1,15 @@
 package app
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
 
 func init() {
-	fmt.Println("init")
+	viper.SetConfigFile(".env")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
